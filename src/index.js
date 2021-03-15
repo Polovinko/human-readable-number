7932bfaa3eb06e741abaeb8cbd96ebcfb0f7b03f
Polovinko <ospolovinko@gmail.com>
2021-03-15 23:51:00 +0300

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function toReadable (number) {
-  
+  let massiveType1=["zero","one","two","three", "four", "five", "six", "seven","eight","nine"];
+  let massiveType2=["ten","eleven","twelve", 'thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+  let massiveType3=["twenty","thirty", 'forty','fifty','sixty','seventy','eighty','ninety'];
+  let num=""+number;
+  let str="";
+  if (num.length==1){
+    str+=massiveType1[number];
+  } else if (num.length==2){
+      if (num[0]=="1"){
+          str=massiveType2[+num[1]];
+      }else{
+          str=massiveType3[num[0]-2];
+          if (num[1]!=0) str+=" "+massiveType1[num[1]];
+      }
+  }else if (num.length==3){
+      str=massiveType1[num[0]]+" hundred";
+    if (num[1]=="1"){
+        str+=" "+massiveType2[+num[2]];
+    }else if (num[1]=="0"&&num[2]!=0){
+        str+=" "+massiveType1[num[2]];
+    }else if(num[1]!="0"){
+        str+=" "+massiveType3[num[1]-2];
+        if (num[2]!=0) str+=" "+massiveType1[num[2]];
+    }
+  }
+  return str;
 }
